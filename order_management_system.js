32c8072ba2f26a6ea6b78ede14dd162ae392695b
warnerfleming <fleming14@usf.edu>
2024-09-19 23:16:33 -0400

    Initialize inventory with product objects

--- order_management_system.js
+++ order_management_system.js
@@ -1,1 +1,7 @@
-
+//Task 1
+const inventory = [
+    {name: `pizza`, price: `12`, quantity: `30`},
+    {name: `sandwich`, price: `10`, quantity: `50`},
+    {name: `fries`, price: `4`, quantity: `100`},
+    {name: `soda`, price: `3`, quantity: `60`},
+]
